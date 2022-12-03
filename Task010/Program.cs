9a667866e4178bd295d530ba28c26b522b00f46c
Igor <klevzhits94@icloud.com>
2022-12-03 03:45:19 +0300

    Add checking information

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+адача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Write("Введите трехзначное число: ");
+string input = Console.ReadLine() ?? "";
+int number;
+if(int.TryParse(input,out number))
+{
+    if ((number < 100) || (number > 999))
+    {
+        Console.WriteLine("Число не трехзначное!");
+    }
+    else
+    {
+        Console.WriteLine("Ок!");
+    }
+}
+else
+{
+    Console.WriteLine("Данные введены некорректно!");
+}
