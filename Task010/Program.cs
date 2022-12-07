79e2d5817fbe13ae4f2b266d0dc2afa61a68e1d4
Igor <klevzhits94@icloud.com>
2022-12-07 11:41:22 +0200

    Commit Task010

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -1,24 +1,48 @@
 ﻿/*
-адача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 456 -> 5
 782 -> 8
 918 -> 1
 */
-Console.Write("Введите трехзначное число: ");
-string input = Console.ReadLine() ?? "";
-int number;
-if(int.TryParse(input,out number))
+bool checkInput(string input) //Функция проверки корректности введенных данных
 {
-    if ((number < 100) || (number > 999))
+    int number;
+    if (int.TryParse(input, out number))
     {
-        Console.WriteLine("Число не трехзначное!");
+        return true;
     }
     else
     {
-        Console.WriteLine("Ок!");
+        Console.WriteLine("Данные введены некорректно!");
+        return false;
     }
 }
-else
+
+int findSecondNumber(int number) //Нахождение второго числа если есть
 {
-    Console.WriteLine("Данные введены некорректно!");
+    if ((number > -1000) && (number < -99) || (number > 99) && (number < 1000))
+    {
+        number %= 100;
+        number /= 10;
+        if (number < 0)
+            number *= -1;
+        return number;
+    }
+    else
+    {
+        Console.WriteLine("Число не трехзначное!");
+        return 404; //Ошибка
+    }
 }
+
+Console.Write("Введите трехзначное число: ");
+string input = Console.ReadLine() ?? "";
+if (checkInput(input)) //Если данные введены корректно
+{
+    int number = int.Parse(input);
+    if (findSecondNumber(number) != 404) // Если нет ошибки
+    {
+        Console.WriteLine($"Вторая цифра числа {input} это - {findSecondNumber(number)}"); 
+    }
+}
+
